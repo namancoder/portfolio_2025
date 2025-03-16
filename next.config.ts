c9126234871aa7697dbe1016be88d4801612cc8c
Naman <namanitsofficial@gmail.com>
2025-03-16 13:53:24 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "portfolio_2025";
 const nextConfig: NextConfig = {
-  // basePath: "/abcd",
+  reactStrictMode: true,
+  basePath: isProd ? `/${repoName}` : "", // Replace with your repo name
+  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true, // Required for GitHub Pages
   },
+  // basePath: "/abcd",
+  // images: {
+  //   dangerouslyAllowSVG: true,
+  //   contentDispositionType: "attachment",
+  //   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  // },
 };
 
 export default nextConfig;
